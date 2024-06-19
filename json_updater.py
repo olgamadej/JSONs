f2569d11e52a897b261e662fc85f0b91d4a4e57d
Olga O <42141921+olgamadej@users.noreply.github.com>
2024-06-20 01:50:41 +0200

    updated ids

--- json_updater.py
+++ json_updater.py
@@ -1,14 +1,13 @@
 import json
 
 #Load the JSON data from a file
-with open('french.json', 'r', encoding='utf-8') as file:
+with open('updatedfrench.json', 'r', encoding='utf-8') as file:
 	data = json.load(file)
 
-#Ensure all 'subject' fields are arrays
-for entry in data:
-	if 'subject' in entry and isinstance(entry['subject'], str):
-		entry['subject'] = [entry['subject']]
+#Create ids for all objects
+for i, entry in enumerate(data, start=1):
+	entry['id'] = i
 
 #Save the updated JSON data back to the file
-with open('updatedfrench.json', 'w', encoding='utf-8') as file:
+with open('withids.json', 'w', encoding='utf-8') as file:
 	json.dump(data, file, ensure_ascii=False, indent=4)
